417d4002148cacdab40c7dcea61d039eccef5053
carlostojal <carlos.tojal@hotmail.com>
2023-09-13 22:20:31 +0100

    add a connection method

--- include/carnary/CARnaryClient.h
+++ include/carnary/CARnaryClient.h
@@ -5,12 +5,33 @@
 #ifndef CARNARY_LIB_CARNARYCLIENT_H
 #define CARNARY_LIB_CARNARYCLIENT_H
 
+#include <carnary/Utils.h>
+#include <carnary/protocol.h>
+
 namespace carnary::client {
 
+    /*! \brief Main client implementation. */
     class CARnaryClient {
 
     public:
-        tryConnect();
+
+        /*! \brief Create a server socket.
+         *
+         * @return Socket file descriptor.
+         */
+        int tryConnect(std::uint8_t numTries = 1);
+
+        /*! \brief Start negotiation with the daemon.
+         *
+         * @param daemonfd Negotiation file descriptor obtained using "tryConnect"
+         * @param serviceName Service description.
+         * @param minHeartbeatRate Minimum heartbeat rate this system should provide.
+         * @param myPID PID of this system, obtained using "getpid()" system call.
+         */
+        void negotiate(int daemonfd, const std::string& serviceName, std::uint16_t minHeartbeatRate, pid_t myPID);
+
+        /*! \brief Close socket file descriptors. */
+        void cleanup();
     };
 
 } // carnary::client
--- src/CARnaryClient.cpp
+++ src/CARnaryClient.cpp
@@ -2,9 +2,42 @@
 // Created by carlostojal on 13-09-2023.
 //
 
-#include "CARnaryClient.h"
+#include <carnary/CARnaryClient.h>
 
-namespace carnary {
-    namespace client {
-    } // carnary
-} // client
+namespace carnary::client {
+    int CARnaryClient::tryConnect(std::uint8_t numTries) {
+        // negotiation socket file descriptor
+        int negotiationfd;
+
+        // heartbeat socket file descriptor
+        int heartbeatfd;
+
+        for(std::uint8_t n = 0; n < numTries; n++) {
+            // create the socket
+            try {
+                negotiationfd = carnary::lib::Utils::createClientSocket("127.0.0.1", DAEMON_TCP_NEGOTIATION_PORT,
+                                                                        lib::TCP_SOCKET);
+            } catch (std::runtime_error &ex) {
+                // if a trouble happened, try again
+                continue;
+            }
+
+            // if execution reaches this point, the socket was created and connected
+            return negotiationfd;
+        }
+
+        // if execution reaches here, the for loop reached its end, which means the number of tries was exceeded
+        throw std::runtime_error("Exceeded number of tries. Daemon may be down.");
+    }
+
+    void CARnaryClient::negotiate(int daemonfd, const std::string &serviceName, std::uint16_t minHeartbeatRate,
+                                  pid_t myPID) {
+
+        // TODO
+    }
+
+    void CARnaryClient::cleanup() {
+        // TODO
+    }
+
+} // carnary::client
