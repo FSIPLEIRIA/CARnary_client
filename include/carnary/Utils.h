a0feb6e70c0e5d86bc20afa0d755701c34936286
carlostojal <carlos.tojal@hotmail.com>
2023-09-13 22:19:48 +0100

    move the utils from the daemon to the library. added separate methods for client and server sockets

--- include/carnary/Utils.h
+++ include/carnary/Utils.h
@@ -5,22 +5,50 @@
 #ifndef CARNARY_SERVER_UTILS_H
 #define CARNARY_SERVER_UTILS_H
 
-enum socket_type_t {
-    TCP_SOCKET,
-    UDP_SOCKET
-};
+#include <iostream>
+#include <stdexcept>
+#include <cstring>
+#include <cerrno>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string>
 
-namespace carnary::server {
+namespace carnary::lib {
+
+    enum socket_type_t {
+        TCP_SOCKET,
+        UDP_SOCKET
+    };
+
     class Utils {
 
     public:
-        /*! \brief Create a socket.
+
+        /*! \brief Create a server socket.
          *
-         * @param port The port to use.
-         * @param type The type of socket (TCP_SOCKET/UDP_SOCKET)
-         * @return The socket file descriptor
+         * @param port The port to listen on.
+         * @param type The type of socket (TCP_SOCKET/UDP_SOCKET).
+         * @return The socket file descriptor.
          */
-        static int createSocket(int port, socket_type_t type);
+        static int createServerSocket(int port, socket_type_t type);
+
+        /*! \brief Create a client socket.
+         *
+         * @param ipAddress The IP address to connect to.
+         * @param port The port to connect to.
+         * @param type The type of socket (TCP_SOCKET/UDP_SOCKET).
+         * @return The socket file descriptor.
+         */
+        static int createClientSocket(const std::string& ipAddress, int port, socket_type_t type);
+
+        /*! \brief Create a generic socket (not server nor client).
+         *
+         * @param type The type of socket (TCP_SOCKET/UDP_SOCKET).
+         * @return The socket file descriptor.
+         */
+        static int createSocket(socket_type_t type);
     };
 }
 
--- src/Utils.cpp
+++ src/Utils.cpp
@@ -2,50 +2,88 @@
 // Created by carlostojal on 12-09-2023.
 //
 
-#include "Utils.h"
-#include <iostream>
-#include <stdexcept>
-#include <cstring>
-#include <cerrno>
-#include <unistd.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
+#include <carnary/Utils.h>
 
-int carnary::server::Utils::createSocket(int port, socket_type_t type) {
+namespace carnary::lib {
 
-    // create the socket file descriptor:
-    // AF_INET means IPv4
-    // SOCK_STREAM means TCP. To use UDP, would use SOCK_DGRAM instead
-    // 0 is the protocol value for IP
-    int sockfd = socket(AF_INET, type == TCP_SOCKET ? SOCK_STREAM : SOCK_DGRAM, 0);
+    int Utils::createServerSocket(int port, carnary::lib::socket_type_t type) {
 
-    // check the socket creation for errors
-    if (sockfd == -1) {
-        std::cerr << strerror(errno) << std::endl;
-        throw std::runtime_error("Error creating the negotiation file descriptor");
+        int sockfd = -1;
+
+        // create a generic socket
+        try {
+            sockfd = Utils::createSocket(type);
+        } catch(std::runtime_error& ex) {
+            throw ex;
+        }
+
+        struct sockaddr_in serverAddr{};
+
+        // pre-fill the address with zeros
+        memset(&serverAddr, 0, sizeof(struct sockaddr_in));
+
+        // fill the address structure
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+        serverAddr.sin_port = htons(port);
+
+        // bind the socket to the address
+        if (bind(sockfd, (struct sockaddr *) &serverAddr, sizeof(struct sockaddr_in)) < 0) {
+            std::cerr << strerror(errno) << std::endl;
+            throw std::runtime_error("Error binding the address to the socket");
+        }
+
+        // listen to uncoming clients
+        if (listen(sockfd, 20) < 0) {
+            std::cerr << strerror(errno) << std::endl;
+            throw std::runtime_error("Error listening for clients");
+        }
+
+        return sockfd;
     }
 
-    struct sockaddr_in serverAddr{};
+    int Utils::createClientSocket(const std::string& ipAddress, int port, carnary::lib::socket_type_t type) {
 
-    // pre-fill the address with zeros
-    memset(&serverAddr, 0, sizeof(struct sockaddr_in));
+        int sockfd = -1;
 
-    // fill the address structure
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serverAddr.sin_port = htons(port);
+        try {
+            sockfd = Utils::createSocket(type);
+        } catch(std::runtime_error& ex) {
+            throw ex;
+        }
 
-    // bind the socket to the address
-    if (bind(sockfd, (struct sockaddr *) &serverAddr, sizeof(struct sockaddr_in)) < 0) {
-        std::cerr << strerror(errno) << std::endl;
-        throw std::runtime_error("Error binding the address to the socket");
+        struct sockaddr_in serverAddr{};
+
+        // pre-fill the address with zeros
+        memset(&serverAddr, 0, sizeof(struct sockaddr_in));
+
+        // fill the address structure
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_addr.s_addr = inet_addr(ipAddress.c_str());
+        serverAddr.sin_port = htons(port);
+
+        // connect
+        if(connect(sockfd, (struct sockaddr*) &serverAddr, sizeof(struct sockaddr_in)) < 0) {
+            throw std::runtime_error("Error connecting to the server!");
+        }
+
+        return sockfd;
     }
 
-    // listen to uncoming clients
-    if (listen(sockfd, 20) < 0) {
-        std::cerr << strerror(errno) << std::endl;
-        throw std::runtime_error("Error listening for clients");
-    }
+    int Utils::createSocket(socket_type_t type) {
 
-    return sockfd;
+        // create the socket file descriptor:
+        // AF_INET means IPv4
+        // SOCK_STREAM means TCP. To use UDP, would use SOCK_DGRAM instead
+        // 0 is the protocol value for IP
+        int sockfd = socket(AF_INET, type == TCP_SOCKET ? SOCK_STREAM : SOCK_DGRAM, 0);
+
+        // check the socket creation for errors
+        if (sockfd == -1) {
+            std::cerr << strerror(errno) << std::endl;
+            throw std::runtime_error("Error creating the negotiation file descriptor");
+        }
+
+        return sockfd;
+    }
 }
